60d00a34104093e6af64345bb1f69205bf727188
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-03-12 22:09:59 +0100

    improving http communications between services

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -9,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -17,13 +20,23 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
-try
-{
-    await DBInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    System.Console.WriteLine(e.Message);
-}
+
+app.Lifetime.ApplicationStarted.Register(async () => {
+    try
+    {
+        await DBInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        System.Console.WriteLine(e.Message);
+    }   
+});
+
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
--- src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6711dd3b0e4205db1767069d87d6e24df476ee84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964f52f0d89929535d31f96101fe182e387b18ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
