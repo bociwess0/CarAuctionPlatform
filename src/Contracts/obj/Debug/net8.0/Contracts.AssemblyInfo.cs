2a5ca9b26245888ee259252f8ff00123d27f267c
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-03-22 22:14:20 +0100

    using message retries

--- src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a7fd15fe24e599af4e133af3eeac50957640bcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d304fcddcd9294fabdd6dea099f09d57cd198476")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
