e85a70322313a48d07bb7893fc1fa7f921a37a10
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-03-23 21:37:13 +0100

    adding registration page

--- src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9049ffc5aa9439fddb7da20675b3da6d358d3d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a96b26dec37d22f9bd8c51b6886012cabf4ed171")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
