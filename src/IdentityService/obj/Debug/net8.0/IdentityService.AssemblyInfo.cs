dbbca65a8e7dd07c0bde2b51795a3ac8a27b3d55
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-04-01 21:25:47 +0200

    allowing token request

--- src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a96b26dec37d22f9bd8c51b6886012cabf4ed171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e85a70322313a48d07bb7893fc1fa7f921a37a10")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
