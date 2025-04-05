7ab750f2db6d6de63d502fc210b4664483ab08a6
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-04-05 22:14:00 +0200

    adding search consumers

--- src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379de87f47e3f953f2f7387a2244a4b2a12e177c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472be00583de13bb0e5ecc5c9d06a24f96e8b061")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
