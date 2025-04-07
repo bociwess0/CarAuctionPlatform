ef296f4cc666d125ec37a2f37b5ac1383d36bd80
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-04-07 21:23:10 +0200

    getting data from API

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Listings from "./auctions/Listings";
 
 export default function Home() {
   console.log("Server component");
   
   return (
     <div>
-      Content goes here!
+      <Listings />
     </div>
   );
 }
