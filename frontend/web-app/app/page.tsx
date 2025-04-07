560e97a8f3e8e941c4643846c9864866cb7f393a
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-04-07 21:49:10 +0200

    styling auction cards

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import Listings from "./auctions/Listings";
 
-export default function Home() {
-  console.log("Server component");
-  
+export default function Home() {  
   return (
     <div>
       <Listings />
