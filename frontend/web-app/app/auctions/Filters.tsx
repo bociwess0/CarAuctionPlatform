39dd2dd1fe84d0c7fe697d5bd61719ada47655f7
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-04-10 21:18:04 +0200

    adding filtering functionality

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -2,7 +2,8 @@ import { useParamsStore } from '@/hooks/useParamsStore';
 import { Button, ButtonGroup } from 'flowbite-react';
 import React from 'react'
 import { AiOutlineClockCircle, AiOutlineSortAscending } from 'react-icons/ai';
-import { BsFillStopCircleFill } from 'react-icons/bs';
+import { BsFillStopCircleFill, BsStopwatchFill } from 'react-icons/bs';
+import { GiFinishLine, GiFlame } from 'react-icons/gi';
 
 
 
@@ -12,6 +13,24 @@ const orderButtons = [
   {
     label: "Alphabetical",
     icon: AiOutlineSortAscending,
+    value: 'make'
+  },
+  {
+    label: "Ending < 6 hours",
+    icon: GiFinishLine,
+    value: 'endingSoon'
+  },
+  {
+    label: "Completed",
+    icon: BsStopwatchFill,
+    value: 'finished'
+  },
+]
+
+const filterButtons = [
+  {
+    label: "Live Auctions",
+    icon: GiFlame,
     value: 'make'
   },
   {
@@ -31,21 +50,36 @@ export default function Filters() {
   const pageSize = useParamsStore(state => state.pageSize);
   const setParams = useParamsStore(state => state.setParams);
   const orderBy = useParamsStore(state => state.orderBy);
+  const filterBy = useParamsStore(state => state.filterBy);
 
   return (
     <div className='flex justify-between items-center mb-4'>
 
-      <div>
-        <span className='uppercase text-sm text-gray-500 mr-2'>Order By</span>
-        <ButtonGroup>
-          {orderButtons.map(({ label, icon: Icon, value }) =>(
-            <Button key={value} onClick={() => setParams({orderBy: value})}  
-                color={`${orderBy === value ? 'red' : 'gray'}`}>
-                  <Icon className='mr-3 h-4 w-4'></Icon>
-              {label}
-            </Button>
-          ))}
-        </ButtonGroup>
+      <div className='flex justify-between items-center gap-20'>
+        <div>
+          <span className='uppercase text-sm text-gray-500 mr-2'>Filter By</span>
+          <ButtonGroup>
+            {filterButtons.map(({ label, icon: Icon, value }) =>(
+              <Button key={value} onClick={() => setParams({filterBy: value})}  
+                  color={`${filterBy === value ? 'red' : 'gray'}`}>
+                    <Icon className='mr-3 h-4 w-4'></Icon>
+                {label}
+              </Button>
+            ))}
+          </ButtonGroup>
+        </div>
+        <div>
+          <span className='uppercase text-sm text-gray-500 mr-2'>Order By</span>
+          <ButtonGroup>
+            {orderButtons.map(({ label, icon: Icon, value }) =>(
+              <Button key={value} onClick={() => setParams({orderBy: value})}  
+                  color={`${orderBy === value ? 'red' : 'gray'}`}>
+                    <Icon className='mr-3 h-4 w-4'></Icon>
+                {label}
+              </Button>
+            ))}
+          </ButtonGroup>
+        </div>
       </div>
 
       <div>
