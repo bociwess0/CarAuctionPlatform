f820857b2bf9d67e24dff6f1f3ab05a8e558f305
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-05-11 15:04:02 +0200

    adding userActions droprown

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,15 +1,25 @@
 'use client'
 
 import { useParamsStore } from '@/hooks/useParamsStore'
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { AiOutlineCar } from 'react-icons/ai'
 
 export default function Logo() {
 
+    const router = useRouter();
+    const pathName = usePathname();
+
+    function doReset() {
+        if(pathName !== '/') router.push('/');
+        reset();
+    }
+
     const reset = useParamsStore(state => state.reset);
         
     return (
-        <div onClick={reset} className='flex cursor-pointer items-center gap-2 text-3xl font-semibold text-red-500'>
+        <div onClick={doReset} className='flex cursor-pointer items-center gap-2 text-3xl font-semibold text-red-500'>
             <AiOutlineCar  size={34}/>
             <div>Car Auctions</div>
         </div>
