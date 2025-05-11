a53e4e0460637c56273813a6a6021579a09dae12
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-05-11 20:38:42 +0200

    getting the access token to use to authenticate to our resource server

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { auth } from "@/auth";
 import { Auction, PagedResult,  } from "@/types";
 
 export async function getData(query: string) : Promise<PagedResult<Auction>>{
@@ -15,9 +16,14 @@ export async function updateAuctionTest() {
         mileage: Math.floor(Math.random() * 10000) + 1
     }
 
-    const res = await fetch(`http://localhost:6001/auctions`, {
+    const session = await auth();
+
+    const res = await fetch(`http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c`, {
         method: 'PUT',
-        headers: {},
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + session?.accessToken,
+        },
         body: JSON.stringify(data)
     })
 
@@ -28,6 +34,6 @@ export async function updateAuctionTest() {
         }
     }
 
-    return res.json();
+    return res.statusText;
 
 }
