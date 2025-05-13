d5fd0f5c0888c79cb2daacd9a9143be528a6db96
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-05-13 22:16:16 +0200

    creating an auction form

---
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { Button, TextInput } from 'flowbite-react';
+import React from 'react'
+import { FieldValue, FieldValues, useForm } from 'react-hook-form'
+
+export default function AuctionForm() {
+    const {register, handleSubmit, setFocus, formState: {isSubmitting, isValid, isDirty, errors}} = useForm();
+
+    function onSubmit(data: FieldValues) {
+        console.log(data);
+    }
+
+    return (
+    <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
+        <div className='mb-3 block'>
+            <TextInput 
+                {...register('make', { required: 'Make is required!' })}
+                placeholder='Make'
+                color={errors?.make && 'failure'}
+                // helperText={errors.make?.message as string}
+            />
+        </div>
+        <div className='mb-3 block'>
+        <TextInput 
+                {...register('model', { required: 'Model is required!' })}
+                placeholder='Model'
+                color={errors?.make && 'failure'}
+                // helperText={errors.make?.message as string}
+            />
+        </div>
+        <div className='flex justify-between'>
+            <Button outline color={'gray'}>Cancel</Button>
+            <Button outline disabled={!isValid} type='submit' color={'success'}>Submit</Button>
+        </div>
+    </form>
+    )
+}
