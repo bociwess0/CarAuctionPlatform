c48dcc1816bdaba271ec82926128829761cdb6bb
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-05-20 21:43:48 +0200

    adding toast message

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,9 +1,32 @@
+import { getDetailedViewData } from '@/app/actions/auctionActions'
+import Heading from '@/app/components/Heading';
 import React from 'react'
+import CountdownTimer from '../../CountdownTimer';
+import CardImage from '../../CardImage';
+import DetailedSpecs from './DetailedSpecs';
 
-export default function Details({params}: {params: { id: string }}) {
+export default async function Details({params}: {params: { id: string }}) {
+  const data = await getDetailedViewData(params.id);
   return (
     <div>
-      Details for {params.id}
+      <div className='flex justify-between'>
+        <Heading title={`${data.make} ${data.model}`} />
+      </div>
+      <div className='flex gap-3'>
+        <h3 className='text-2xl font-semibold'>Time remaining:</h3>
+        <CountdownTimer auctionEnd={data.auctionEnd} />
+      </div>
+      <div className='grid grid-cols-2 gap-6 mt-3'>
+        <div className='w-full bg-gray-200 relative aspect-[4/3] aspect-w-16 rounded-lg overflow-hidden'>
+          <CardImage imageUrl={data.imageUrl} />
+        </div>
+        <div className='border-2 rounded-lg p-2 bg-gray-100'>
+          <Heading title='Bids' />
+        </div>
+      </div>
+      <div className='mt-3 grid grid-cols-1 rounded-lg'>
+        <DetailedSpecs auction={data} />
+      </div>
     </div>
   )
 }
