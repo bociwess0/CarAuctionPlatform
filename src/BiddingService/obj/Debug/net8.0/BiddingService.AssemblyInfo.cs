835cfe6dc1f576ce9d2f1e8197c662a6a2c87356
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-05-22 21:50:04 +0200

    adding Background service for the auction finished event

--- src/BiddingService/obj/Debug/net8.0/BiddingService.AssemblyInfo.cs
+++ src/BiddingService/obj/Debug/net8.0/BiddingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0cfffc59989296fc8adfe374b1cbe9821c53a69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706facfc128ce62964a908bee5781ffe3dfbcc6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
