4e370c20d7491b37d2d1fb5feeaa88fa6a65d74a
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-05-27 22:01:23 +0200

    adding conditionals to the form and testing

--- frontend/web-app/app/auctions/details/[id]/BidList.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidList.tsx
@@ -64,7 +64,18 @@ export default function BidList({user, auction}: Props) {
             </div>
 
             <div className='px-2 pb-2 text-gray-500'>
-                <BidForm auctionId={auction.auctionEnd} highBid={highBid} />
+                {!user ? (
+                    <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                        Please login to make bid
+                    </div>
+                ) : user && user.username === auction.seller ? (
+                    <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                        You cannot bid on your own auction!
+                    </div>
+                ) : (
+                    <BidForm auctionId={auction.auctionEnd} highBid={highBid} />
+                )}
+                
             </div>
         
         </div>
